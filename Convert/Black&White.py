9fda4a195f875af16bc024d5dbe60468d89c9943
UnsignedByte <edlam1889@gmail.com>
2018-10-15 19:23:00 -0700

    add bunch stuff

--- Convert/Black&White.py
+++ Convert/Black&White.py
@@ -2,8 +2,8 @@
 # @Date:   15:53:50, 15-Oct-2018
 # @Filename: Black&White.py
 # @Last modified by:   edl
-# @Last modified time: 16:56:19, 15-Oct-2018
-from PIL import Image,ImageDraw,ImageFont
+# @Last modified time: 19:22:48, 15-Oct-2018
+from PIL import Image,ImageDraw,ImageFont,ImageEnhance
 import os
 import math
 #grey=list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
@@ -12,6 +12,9 @@ fpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with open(fpath+"/char.txt", "rb") as file:
     grey=file.read().decode('UTF-8').split(chr(166))
 
+def sigmoidSquish(x):
+    return 1/(1+math.e**(x/-100000))
+
 def isInt(s):
     try:
         int(s)
@@ -19,11 +22,8 @@ def isInt(s):
     except ValueError:
         return False
 
-def GreyScale(img, max):
-    line=""
-    lines=1
+def GreyScale(img, max, fsize):
     w,h=img.size
-    out=""
     h/=(9/6)
     if max is not None:
         f = math.sqrt(max/(h*w))
@@ -31,11 +31,38 @@ def GreyScale(img, max):
         h *=f
     w = math.floor(w)
     h = math.floor(h)
-    # print(w, h, w*h)
-    img=img.resize((w,h), Image.ANTIALIAS)
+    enhancement = ((-sigmoidSquish(w*h)+1)+1)**6
+    print("IMG Sharpness factor: "+str(enhancement))
+
+    out = getLines(ImageEnhance.Sharpness(img.resize((w,h), Image.ANTIALIAS)).enhance(enhancement));
+
+    imgnew = Image.new('L', (w*6, len(out.splitlines())*9), (255))
+    fnt=ImageFont.truetype("/Library/Fonts/Courier New.ttf", 10)
+    d = ImageDraw.Draw(imgnew)
+    d.multiline_text((0, -10), out,fill=(0),font=fnt,spacing=0)
+    if fsize !=(6, 9):
+        w,h=img.size
+        h/=(fsize[1]/fsize[0])
+        if max is not None:
+            f = math.sqrt(max/(h*w))
+            w *=f
+            h *=f
+        w = math.floor(w)
+        h = math.floor(h)
+        enhancement = ((-sigmoidSquish(w*h)+1)+1)**6
+        print("TEXT Sharpness factor: "+str(enhancement))
+
+        out = getLines(ImageEnhance.Sharpness(img.resize((w,h), Image.ANTIALIAS)).enhance(enhancement));
+    return (imgnew, out)
+
+def getLines(img):
+    w,h=img.size
+    line=""
+    lines=1
+    out=""
     for color in img.getdata():
         if len(line)==w:
-            out=out+"\n"+line
+            out=out+"\n"+line.rstrip()
             line=""
             lines+=1
         try:
@@ -46,16 +73,13 @@ def GreyScale(img, max):
             line=line+grey[round(((0.2126*r + 0.7152*g + 0.0722*b)/255)*(len(grey)-1))]
         except TypeError:
             line=line+grey[round((color/255)*(len(grey)-1))]
-    imgnew = Image.new('L', (w*6, lines*9), (255))
-    fnt=ImageFont.truetype("/Library/Fonts/Courier New.ttf", 10)
-    d = ImageDraw.Draw(imgnew)
-    d.multiline_text((0, -10), out,fill=(0),font=fnt,spacing=0)
-    return (imgnew, out)
+    return out
+
 
 while True:
     res = input("Ascii or Unicode:")
     if res.lower() == "ascii":
-        grey = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+        grey = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^'. ")
     elif res.lower() == "unicode":
         pass
     else:
@@ -63,7 +87,7 @@ while True:
         exit(0)
     res = input("Maximum Characters (type \"inf\" for no max):")
     if res.lower() == "inf":
-        max = 4000000
+        max = None
     elif isInt(res.lower()):
         max = int(res)
     else:
@@ -85,19 +109,27 @@ while True:
             except EOFError:
                 break
         gif[0].save(fpath+'/output/%s.gif' %(filename.split(".")[0]),save_all=True,append_images=gif[1:])
-        Image.open(fpath+'/output/%s.gif' %(filename.split(".")[0])).show()
     else:
         try:
             rgb_image = Image.new("RGBA", image.size, "WHITE") # Create a white rgba background
             rgb_image.paste(image, (0, 0), image)              # Paste the image on the background. Go to the links given below for details.
         except ValueError:
             rgb_image = image
+        res = input("Text font width, height (separate with space), \"def\" for default:")
+        if res.lower() == "def":
+            fsize = (6, 9)
+        else:
+            try:
+                fsize = tuple(map(int, res.split(",")))
+            except Exception:
+                print("Invalid Input")
+                exit(0)
         rgb_image.convert('RGB')
-        tupg = GreyScale(rgb_image, max)
+        tupg = GreyScale(rgb_image, max, fsize)
         tupg[0].save(fpath+"/output/%s.png" %(filename.split(".")[0]))
+        print("%s characters written." %len(tupg[1]))
 
         with open(fpath+"/output_text/%s.txt" %(filename.split(".")[0]), "w") as f:
             f.write(tupg[1])
-        Image.open(fpath+'/output/%s.png' %(filename.split(".")[0])).show()
     # except Exception:
     #     print("incompatible/missing file")
